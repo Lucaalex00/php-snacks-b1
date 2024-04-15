5ddf75b6448ceb0c506d51316a70d36d860bd627
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-04-15 11:49:31 +0200

    first commit

---
+++ snack-1/index.php
@@ -0,0 +1,6 @@
+/*
+Creiamo un array contente le partite di basket di un ipotetica tappa del calendario.
+ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra osptite.
+Sstampiamo a schermo tutte le partite con questo schema:
+Olimpia MIliano - Cantù| 55-60
+*/
